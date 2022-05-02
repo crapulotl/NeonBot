1edc4680ac7368b93055292d3ea3ea1c343cd6ea
crapulotl <91688521+crapulotl@users.noreply.github.com>
2022-05-03 01:44:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('fs');
+const { stringify } = require('querystring');
 
 let author = '';
 let command = '';
@@ -51,10 +52,27 @@ function calcAmount(inputAmount) {
     }
 }
 
-function checkValidity(inputCommand) {
-    let tempObj = readRecord('commands');
+function transfer(jsonData, author, item, recipient, quantity) {
+    if (!(jsonData[author][item] >= 0)) { return false }
+    if (!(jsonData[recipient][item] >= 0)) { jsonData[recipient][item] = 0 }
 
-    for (let i = 0; i < 2; i++) {
+    try {
+        if (jsonData[author][item] >= quantity) {
+
+            jsonData[author][item] = parseInt(jsonData[author][item] - quantity);
+            jsonData[recipient][item] = parseInt(jsonData[recipient][item] + quantity);
+
+            updateRecord('record', '.json', JSON.stringify(jsonData));
+            return true
+        }
+        else { return false }
+    } catch (error) { return false }
+}
+
+function checkValidity(inputCommand, fileName) {
+    let tempObj = readRecord(fileName);
+
+    for (let i = 0; i < tempObj['length']; i++) {
         try {
             if (tempObj['command'][i] === inputCommand) { return true }
         }
@@ -62,8 +80,20 @@ function checkValidity(inputCommand) {
             return false
         }
     }
-
     return false;
+}
+
+function checkKeyValidity(inputKey, inputValue) {
+    let jsonData = readRecord('record')
+
+    try {
+        let temp = jsonData[inputKey][inputValue]
+        return true
+    } catch (error) {
+        jsonData[inputKey] = {};
+        updateRecord('record', '.json', JSON.stringify(jsonData))
+        return false
+    }
 }
 
 client.once('ready', () => {
@@ -71,54 +101,101 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', message => {
-    msg = message.content.split(' ');
-    command = msg[0].substring(1);
+    if (!message.content.startsWith('+')) { return }
 
-    if (!msg[0].startsWith('+')){
-        return
-    }
-    
-    if (!(checkValidity(command))) {
-        message.channel.send(`"${command}" is not a valid command`);
-        return
-    }
+    try {
+        msg = message.content.split(' ');
+        command = msg[0].substring(1);
+        jsonDataOld = readRecord('record');
 
-    if (command === 'pay') {
         try {
+            if (checkValidity(command, 'recipient_based_commands') && !(msg[1].substring(1, 2) === '@')) {
+                message.channel.send(`"${msg[1]}" doesn't exist`);
+                return
+            }
+        } catch (error) {
+            message.channel.send('Sorry, something went wrong')
+        }
+
+
+        if (!(checkValidity(command, 'commands')) && !(checkValidity(command, 'recipient_based_commands'))) {
+            message.channel.send(`"${command}" is not a valid command`);
+            return
+        }
+
+        if (command === 'pay') {
             calcAmount(msg[2]);
 
             let recipient = msg[1];
             author = message.author.toString();
+
+            checkKeyValidity(author, 'money');
+            checkKeyValidity(recipient, 'money');
             let jsonData = readRecord('record');
-            jsonDataOld = readRecord('record');
 
-            if ((jsonData[author]['money'] >= amountPayedn) && (amountPayedn > 0)) {
-
-                jsonData[author]['money'] = parseInt(jsonData[author]['money'] - amountPayedn);
-                jsonData[recipient]['money'] = parseInt(jsonData[recipient]['money'] + amountPayedn);
-
-                updateRecord('record', '.json', JSON.stringify(jsonData));
-
-                if (author === recipient){ message.channel.send(`Nice try ${message.author.username}`); }
+            if (amountPayedn > 0 && transfer(jsonData, author, 'money', recipient, amountPayedn)) {
+                if (author === recipient) { message.channel.send(`Nice try ${message.author.username}`); }
                 else { message.channel.send(`You just payed ${recipient} ${amountPayed}N`); }
-                
+
             }
             else {
                 if (amountPayedn > 0) { message.channel.send('You do not have enough Neons') }
                 else { message.channel.send('Please transfer a larger amount') }
             }
         }
-        catch (error) {
-            message.channel.send('Sorry, an error occured');
-            console.log(error);
+
+        if (command === 'balance') {
+            let jsonData = readRecord('record');
+            let author = message.author.toString();
+            let item = message.content.split('+balance ')[1];
+
+            try {
+                if (item === 'money') {
+                    message.channel.send(`${author}. Your balance is: ${jsonData[author]['money'] / 100} Neons`)
+                } else {
+                    if (!(jsonData[author][item] === undefined)) {
+                        message.channel.send(`${author}. You have ${jsonData[author][item]} ${item}`)
+                    } else { message.channel.send(`${author}. You have no ${item}`) }
+                }
+            }
+            catch (error) {
+                message.channel.send(`${author}. You have no ${item.substring}`);
+                console.log(error);
+                return
+            }
+        }
+
+        if (command === 'give') {
+            let author = message.author.toString();
+            let recipient = msg[1];
+            let quantity = msg[2];
+            let item = message.content.split(`+give ${recipient} ${quantity} `)[1].substring(0);
+
+            checkKeyValidity(author, item);
+            checkKeyValidity(recipient, item);
+
+            let jsonData = readRecord('record');
+
+            try {
+                if (quantity > 0 && transfer(jsonData, author, item, recipient, quantity)) {
+                    if (author === recipient) { message.channel.send(`Nice try ${message.author.username}`); }
+                    else { message.channel.send(`You just gave ${recipient} ${quantity} ${item}`); }
+
+                }
+                else {
+                    if (quantity > 0) { message.channel.send(`You do not have enough ${item}`) }
+                    else { message.channel.send(`You can't send 0 ${item}`) }
+
+                };
+            }
+            catch (error) {
+                message.channel.send(`Sorry ${message.author.username}, something seems to have went wrong`);
+            }
         }
     }
-
-    if (command === 'balance'){
-        let jsonData = readRecord('record');
-        let author = message.author.toString();
-
-        message.channel.send(`${author}. Your balance is: ${jsonData[author]['money'] / 100} Neons`)
+    catch (error) {
+        console.log(error)
+        return
     }
 });
 
